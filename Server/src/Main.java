469a20d7a3ddd68f07f6794154d150986f9ae3dd
Zojae031 <edgar3@naver.com>
2019-12-26 16:27:55 +0900

    Repository Inject

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,5 +1,8 @@
+import controller.Controller;
+import inject.Injection;
+
 public class Main {
     public static void main(String[] args) {
-        
+        new Controller(Injection.getInstance().injectRepository());
     }
 }
---
+++ Server/src/controller/Controller.java
@@ -0,0 +1,12 @@
+package controller;
+
+import data.Repository;
+
+public class Controller {
+    private Repository repository;
+
+    public Controller(Repository repository) {
+        this.repository = repository;
+    }
+
+}
