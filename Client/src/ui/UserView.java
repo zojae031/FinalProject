8e1a1e59917294d1ea1e996580de5406904e244a
Zojae031 <edgar3@naver.com>
2019-12-27 14:59:46 +0900

    Define UserView Interface

--- Client/src/ui/UserView.java
+++ Client/src/ui/UserView.java
@@ -1,5 +1,22 @@
 package ui;
 
+import data.dao.ItemDao;
+
+import java.util.Vector;
+
 public interface UserView {
+    void updateItemLists(Vector<ItemDao> ItemLists);
+
+    void updateSelectedLists(Vector<ItemDao> selectedItems);
+
+    void updateInsertMoney(String money);
+
+    void updateChangesMoney(String money);
+
+    void updateTotalMoney(String money);
+
+    void showAdminDialog();
+
+    void showUserInterface();
 
 }
