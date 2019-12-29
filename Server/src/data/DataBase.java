4a2695ca844bb9ede43b26215ccfc407a035561e
HomeZe <55151796+HomeZe@users.noreply.github.com>
2019-12-29 22:18:25 +0900

    Add get Product And Ingredient Data with JsonArray

--- Server/src/data/DataBase.java
+++ Server/src/data/DataBase.java
@@ -1,4 +1,5 @@
 package data;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import java.sql.*;
@@ -72,5 +73,53 @@ public class DataBase {
         }
     }//registerIngredient
 
+    public JsonArray getProductArray(){
+        connectDB();
+        String sql = "select * from product";
+        JsonArray result = new JsonArray();
+        try {
+            pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            JsonObject input;
+            while(rs.next()){
+                input = new JsonObject();
+                input.addProperty("PrCode",rs.getString("PrCode"));
+                input.addProperty("PrName",rs.getString("PrName"));
+                input.addProperty("PrPrice",rs.getString("PrPrice"));
+                input.addProperty("PrNumber",rs.getString("PrNumber"));
+                input.addProperty("PrIngredient",rs.getString("PrIngredient"));
+                result.add(input);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeDB();
+        return result;
+    }//getProductArray
+
+    public JsonArray getIngredientArray(){
+        connectDB();
+        String sql = "select * from ingredient";
+        JsonArray result = new JsonArray();
+        try {
+            pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+            JsonObject input;
+            while(rs.next()){
+                input = new JsonObject();
+                input.addProperty("IgCode",rs.getString("IgCode"));
+                input.addProperty("IgName",rs.getString("IgName"));
+                input.addProperty("IgNumber",rs.getString("IgNumber"));
+                input.addProperty("IgPrice",rs.getString("IgPrice"));
+                result.add(input);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        closeDB();
+        return result;
+    }//getIngredientArray
 
 }// Class DataBase
