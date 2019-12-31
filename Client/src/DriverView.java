83078f7da0eb78ce2d38b4bcb8b728a3d7b1bc7f
jungyr24 <37285946+jungyr24@users.noreply.github.com>
2019-12-31 11:24:38 +0900

    driver 클래스 추가

---
+++ Client/src/DriverView.java
@@ -0,0 +1,14 @@
+import controller.Controller;
+import data.RepositoryImpl;
+import data.ServerConnection;
+import ui.UserView;
+import ui.UserViewImpl;
+
+import java.util.Vector;
+
+public class DriverView {
+    public static void main(String[] args) {
+        UserViewImpl view = new UserViewImpl();
+    }
+
+}
