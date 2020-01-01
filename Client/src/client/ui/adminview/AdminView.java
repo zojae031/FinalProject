0107167f16e82e43aa930d8cb34d57c495f1cb00
Zojae031 <edgar3@naver.com>
2020-01-01 21:33:21 +0900

    addAdminListener

--- Client/src/client/ui/adminview/AdminView.java
+++ Client/src/client/ui/adminview/AdminView.java
@@ -1,12 +1,10 @@
 package client.ui.adminview;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionListener;
 
 public interface AdminView {
     JPanel adminStartPanel = new JPanel();
-
 
 
     void addAdminListener(ActionListener listener);
